a70070d88dbc34920ffe2d43a2d99aa3ca85d85d
PonomarenkoNina <ninulka@ukr.net>
2024-03-23 17:03:05 +0200

    declare methods related to the view object

--- battleship_real.js
+++ battleship_real.js
@@ -1,1 +1,12 @@
-
+//Oголошуємо методи, які стосуються об'єкту подання - declare methods related to the view object
+var view = {
+  displayMessage: function (msg) {
+    //код буде приведений пізніше
+  },
+  userMiss: function (location) {
+    //код буде приведений пізніше
+  },
+  userHit: function (location) {
+    //код буде приведений пізніше
+  },
+};
