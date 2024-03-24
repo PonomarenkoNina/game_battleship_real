cc84c3fadb70ebe3b10dbcd6ad32455b2895f803
PonomarenkoNina <ninulka@ukr.net>
2024-03-24 15:45:20 +0200

    added exersise and delete test shot

--- battleship_real.js
+++ battleship_real.js
@@ -14,13 +14,7 @@ var view = {
     cell.setAttribute("class", "miss");
   },
 };
-//Тест - постріл гравця А0, D4, F5, B2, C5, C6
-view.displayMiss("00"); //А0
-view.displayHit("34"); //D4
-view.displayMiss("55"); //F5
-view.displayHit("12"); //B2
-view.displayMiss("25"); //C5
-view.displayHit("26"); //C6
+
 view.displayMessage("Tap, tap, is this thing on?");
 
 //Об'єкт моделі(зберігаємо поточний стан гри - позиції кораблів, влучання і лічильник потоплених кораблів)
@@ -34,3 +28,22 @@ var ships = [
   { locations: ["32", "33", "34"], hits: ["", "", ""] }, //Другий корабль D2,D3,D4
   { locations: ["63", "64", "65"], hits: ["", "", "hit"] }, //Третій корабль G3,G4,G5
 ];
+//Exersise координати пострілів: A6, B3, C4, D1, B0, D4, F0, A1, C6, B1, B2, E4, B6
+var ships = [
+  { locations: ["06", "16", "26"], hits: ["hit", "hit", "hit"] }, //Перший корабль A6,B6,C6
+  { locations: ["24", "34", "44"], hits: ["hit", "hit", "hit"] }, //Другий корабль C4,D4,E4
+  { locations: ["10", "11", "12"], hits: ["hit", "hit", "hit"] }, //Третій корабль B0,B1,B2
+];
+view.displayHit("06"); //А6
+view.displayMiss("13"); //B3
+view.displayHit("24"); //C4
+view.displayMiss("31"); //D1
+view.displayHit("10"); //B0
+view.displayHit("34"); //D4
+view.displayMiss("50"); //F0
+view.displayMiss("01"); //A1
+view.displayHit("26"); //C6
+view.displayHit("11"); //B1
+view.displayHit("12"); //B2
+view.displayHit("44"); //E4
+view.displayHit("16"); //B6
