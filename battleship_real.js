1f040ff764fc61d53eef91a2d5585bae594d9537
PonomarenkoNina <ninulka@ukr.net>
2024-03-24 15:00:47 +0200

    presented the positions of the ships in the model

--- battleship_real.js
+++ battleship_real.js
@@ -1,4 +1,5 @@
-//Oголошуємо методи, які стосуються об'єкту подання - declare methods related to the view object
+//Визначаємо об'єкт подання(відповідає за оновлення подання)
+//Оголошуємо методи, які стосуються об'єкту подання - declare methods related to the view object
 var view = {
   displayMessage: function (msg) {
     var messageArea = document.getElementById("messageArea");
@@ -21,3 +22,15 @@ view.displayHit("12"); //B2
 view.displayMiss("25"); //C5
 view.displayHit("26"); //C6
 view.displayMessage("Tap, tap, is this thing on?");
+
+//Об'єкт моделі(зберігаємо поточний стан гри - позиції кораблів, влучання і лічильник потоплених кораблів)
+//Кожний корабль уявляємо об'єктом, який зберігає координати зайнятих клітин і кількість влучань
+var ship1 = { locations: ["10", "20", "30"], hits: ["", "", ""] };
+var ship2 = { locations: ["32", "33", "34"], hits: ["", "", ""] };
+var ship3 = { locations: ["63", "64", "65"], hits: ["", "", "hit"] };
+//Створюємо масив для зберігання всіх даних
+var ships = [
+  { locations: ["10", "20", "30"], hits: ["", "", ""] }, //Перший корабль В0,С0,D0
+  { locations: ["32", "33", "34"], hits: ["", "", ""] }, //Другий корабль D2,D3,D4
+  { locations: ["63", "64", "65"], hits: ["", "", "hit"] }, //Третій корабль G3,G4,G5
+];
