46d748fb6607770645f8adec4e43f147b2dc36a1
PonomarenkoNina <ninulka@ukr.net>
2024-03-28 16:06:56 +0200

    controller object added, post-string coordinate processing added, parseGuess function added, testing added

--- battleship_real.js
+++ battleship_real.js
@@ -71,13 +71,74 @@ var model = {
   },
 };
 
-model.fire("53");
-model.fire("06");
-model.fire("16");
-model.fire("26");
-model.fire("34");
-model.fire("24");
-model.fire("44");
-model.fire("12");
-model.fire("11");
-model.fire("10");
+//model.fire("53");
+//model.fire("06");
+//model.fire("16");
+//model.fire("26");
+//model.fire("34");
+//model.fire("24");
+//model.fire("44");
+//model.fire("12");
+//model.fire("11");
+//model.fire("10");
+
+//Визначаємо об'єкт контролера з властивістю guesses (кількість пострілів) та processGuess обробку координат пострілу і передачу їх моделі та перевіряємо завершення гри.
+//Планування коду: напишемо допоміжну функцію parseGuess, пишемо псевдокод + блоксхему
+var controller = {
+  guesses: 0,
+  processGuess: function (guess) {
+    var location = parseGuess(guess); //Метод parseGuess використовується для перевірки введених данних
+    if (location) {
+      this.guesses++;
+      var hit = model.fire(location);
+      if (hit && model.shipSunk === model.numShips) {
+        view.displayMessage(
+          "You sank all my battleships, in " + this.guesses + " guesses"
+        );
+      }
+    }
+  },
+};
+controller.processGuess("A0");
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
+function parseGuess(guess) {
+  //Отримуємо дані від ігрока і перевіряємо їх на дійсність та впевнюємось, що в строчці 2 символи
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+  if (guess === null || guess.length !== 2) {
+    alert("Opps,please enter a letter and a numder on the board.");
+  } else {
+    firstChar = guess.charAt(0); //Вилучається перший символ строки
+    var row = alphabet.indexOf(firstChar); //Отримуємо цифру від 0 до 6
+    var column = guess.charAt(1); //Отримання другого символа
+
+    if (isNaN(row) || isNaN(column)) {
+      //Виявляємо строки та стовпці які не є цифрами
+      alert("Oops, that isn't on the board.");
+    } else if (
+      row < 0 ||
+      row >= model.boardSize ||
+      column < 0 ||
+      column >= model.boardSize
+    ) {
+      alert("Oops, that's off the board!");
+    } else {
+      return row + column; //Строка та стовпець об'єднуються і результат вертається методом
+    }
+  }
+  return null;
+}
+console.log(parseGuess("A0"));
+console.log(parseGuess("B6"));
+console.log(parseGuess("G3"));
+console.log(parseGuess("H0"));
+console.log(parseGuess("A7"));
+//Console JS: 00  16  63  null  null
