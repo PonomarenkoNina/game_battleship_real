b8d21e0eab36fb59ed46c1711d899178707c2ca1
PonomarenkoNina <ninulka@ukr.net>
2024-03-29 16:29:05 +0200

    added ship placements, code that generates random ship positions every time the game starts, a method to avoid overlapping ships

--- battleship_real.js
+++ battleship_real.js
@@ -69,6 +69,48 @@ var model = {
     }
     return true;
   },
+
+  generateShipLocations: function () {
+    var locations;
+    for (var i = 0; i < this.numShips; i++) {
+      do {
+        locations = this.generateShip();
+      } while (this.collision(locations));
+      this.ships[i].locations = locations;
+    }
+  },
+  generateShip: function () {
+    var direction = Math.floor(Math.random() * 2);
+    var row, col;
+
+    if (direction === 1) {
+      row = Math.floor(Math.random() * this.boardSize);
+      col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+    } else {
+      row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+      col = Math.floor(Math.random() * this.boardSize);
+    }
+    var newShipLocations = [];
+    for (var i = 0; i < this.shipLength; i++) {
+      if (direction === 1) {
+        newShipLocations.push(row + "" + (col + i));
+      } else {
+        newShipLocations.push(row + i + "" + col);
+      }
+    }
+    return newShipLocations;
+  },
+  collision: function (locations) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = model.ships[i];
+      for (var j = 0; j < locations.length; j++) {
+        if (ship.locations.indexOf(locations[j]) >= 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  },
 };
 
 //model.fire("53");
@@ -156,7 +198,7 @@ function handleFireButton() {
 function init() {
   var fireButton = document.getElementById("fireButton");
   fireButton.onclick = handleFireButton;
-  var guessInput = document.getElementById("guessInput");
+  var guessInput = document.getElementById("guessInput"); //код для обробки натискання кнопки Enter
   guessInput.onkeypress = handleKeyPress;
 }
 
